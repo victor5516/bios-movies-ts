ae4ab915db400aad7a2c23e194489c819aa6885c
Roberto Carlos Quintero <robertocarlero@gmail.com>
2023-11-04 06:51:48 -0500

    CORS added

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,21 +2,21 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { handleError } from '../handlers/error.handler';
 import { handleResponse } from '../handlers/response.handler';
+import cors from 'cors';
 
 import moviesRoutes from './routes/movies.routes';
 
 const app = express();
 
-
+app.use(cors());
 //midelewares
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 //ruta de prueba
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+	res.send('Hello World!');
 });
 
 app.use('/api/v1', moviesRoutes);
